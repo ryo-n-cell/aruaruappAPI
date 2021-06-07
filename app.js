1b5219d3e1e69e62237fe51e32810a07413e9d19
ryo-n-cell <nakamuraryota582@gmail.com>
2021-06-07 11:34:09 +0900

    kari

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
 var jsonData = {
   key: [
     {
       id: 1,
-      content: "AAAA",
+      content: "ほぼ100%の(ほぼ)にピクッとなる",
     },
     {
       id: 2,
-      content: "BBBBB",
+      content: "コンパイルを実行する時のエンターキーだけなんか強打しちゃう",
     },
     {
       id: 3,
-      content: "CCCCC",
+      content: "手作業で30分掛かるものを3秒で終わらせるために3時間掛ける",
+    },
+    {
+      id: 4,
+      content:
+        "大規模なサービスで障害が置きていると、妙な親近感とワクワクを感じる",
+    },
+    {
+      id: 5,
+      content: "コードを打ち込む時間より調べている時間のほうが長い",
+    },
+    {
+      id: 6,
+      content: "ソースコードに記述するコメントに性格が出る",
+    },
+    {
+      id: 7,
+      content: "帰ろうとするとトラブルが発生する",
+    },
+    {
+      id: 8,
+      content: "キーボードやマウスにはこだわるほうだ",
+    },
+    {
+      id: 9,
+      content: "パソコンのことは何でも知っていると思われてる",
+    },
+    {
+      id: 10,
+      content: "PCが調子悪かったら「とりあえず再起動」",
     },
   ],
 };
@@ -24,4 +53,11 @@ app.get("/", (req, res) => {
 
 app.listen(process.env.PORT || 5000);
 
+console.log("env");
 // herokuにこのアプリをデプロイしてAPIを見る
+// const connection = mysql.createConnection({
+//   host: "サーバ名",
+//   user: "ユーザー名",
+//   password: "パスワード",
+//   database: "データベース"
+// });
