b459e17f493ca2e552cf53705de6887e45a8674b
ryo-n-cell <nakamuraryota582@gmail.com>
2021-11-27 22:20:07 +0900

    Trueの割合を計算する機能を追加※テストDB

--- app/resultsSoFar.js
+++ app/resultsSoFar.js
@@ -1,7 +1,7 @@
 const res = require("../app");
 const mysql = require("mysql2");
 const pool = require("../dbController/pool");
-const returnQIdCalc = [];
+const reqJson = [];
 
 async function resultsSoFar(req, res) {
   const qIdArray = req.query.qId;
@@ -24,76 +24,32 @@ async function resultsSoFar(req, res) {
           console.log("error connecting: " + error.stack);
           return;
         }
-        const dbData = results;
-        dbData.sort(function (a, b) {
-          if (a.question_id < b.question_id) return -1;
-          if (a.question_id > b.question_id) return 1;
-          return 0;
-        });
-        // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
-        const questionIdsArray = [];
-        dbData.forEach((dbData) => {
-          questionIdsArray.push(dbData.question_id);
-        });
-        const untilResultArray = [];
-        // ソートした配列をquestionIDごとの配列としてまとめてuntilResultArrayへ代入する
-        // questionIdsArrayはquestionIDのみを入れた配列
-        for (let qId_i = 0; qId_i <= 9; qId_i++) {
-          // console.log(questionIdsArray);
-          qid_length = questionIdsArray.lastIndexOf(
-            Number(questionIdsArray[qId_i])
+        // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝question_idごとに区切るrecordオブジェクトを作る
+        const carvingDatas = [];
+        for (let qIdArray_i = 0; qIdArray_i <= 9; qIdArray_i++) {
+          let carvingVar = results.filter(
+            (record) => record.question_id === Number(qIdArray[qIdArray_i])
           );
-          let arrayById = [];
-          for (
-            let qid_length_n = 0;
-            qid_length_n <= qid_length;
-            qid_length_n++
-          ) {
-            arrayById.push(dbData[qid_length_n]);
-          }
-          untilResultArray.push(arrayById);
-          questionIdsArray.splice(0, qid_length + 1);
-          dbData.splice(0, qid_length + 1);
+          carvingDatas.push(carvingVar);
         }
-        // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
-        resultRatios = [];
-        untilResultArray.forEach((resultArray) => {
-          let questionParameter = resultArray.length;
-          const trueCount = resultArray.filter(
-            (resultArray) => resultArray.status === 1
+        // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝carvingDatasのtrueの割合を計算
+        carvingDatas.forEach((carvingDataObj) => {
+          let trueSum = carvingDataObj.filter(
+            (DataObjRecord) => DataObjRecord.status === 1
           );
           resultRatio = Math.floor(
-            (trueCount.length / questionParameter) * 100
+            (trueSum.length / carvingDataObj.length) * 100
           );
-          resultRatios.push(resultRatio);
+          reqJson.push({
+            id: carvingDataObj[0].question_id,
+            trueRatio: resultRatio,
+          });
         });
-        reqJson = [
-          { id: `${qIdArray[0]}`, resultRatio: `${resultRatios[0]}` },
-          { id: `${qIdArray[1]}`, resultRatio: `${resultRatios[1]}` },
-          { id: `${qIdArray[2]}`, resultRatio: `${resultRatios[2]}` },
-          { id: `${qIdArray[3]}`, resultRatio: `${resultRatios[3]}` },
-          { id: `${qIdArray[4]}`, resultRatio: `${resultRatios[4]}` },
-          { id: `${qIdArray[5]}`, resultRatio: `${resultRatios[5]}` },
-          { id: `${qIdArray[6]}`, resultRatio: `${resultRatios[6]}` },
-          { id: `${qIdArray[7]}`, resultRatio: `${resultRatios[7]}` },
-          { id: `${qIdArray[8]}`, resultRatio: `${resultRatios[8]}` },
-          { id: `${qIdArray[9]}`, resultRatio: `${resultRatios[9]}` },
-        ];
-        // console.log(reqJson);
+        console.log(reqJson);
         connection.release();
         return res.send(reqJson);
       }
     );
   });
 }
-
-function resultsCalc(qId) {
-  // let resultObj={};
-}
-
 module.exports = resultsSoFar;
-
-// 配列1000個あるもの（result）に対してquestion_idに合致したオブジェクトの配列を作りたい。
-// 　：一回resultをquestion_idを小さい順にソートしておく。
-// 　　次にresultのquestion_idの最初の要素から「外れている」配列要素の番号を探す。
-// 　　その番号から前の配列要素を変数filterRecordに加える。加えたresultの配列要素はその時点で消去する。
