f69ce5d576b5d390b497d497f7397dfd35ed0d8e
ryo-n-cell <nakamuraryota582@gmail.com>
2021-11-30 19:08:58 +0900

    211130本番修正1

--- dbController/pool.js
+++ dbController/pool.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOSTNAME_TEST,
+  user: process.env.DB_USERNAME_TEST,
+  password: process.env.DB_PASSWORD_TEST,
+  database: process.env.DB_NAME_TEST,
 });
 
 module.exports = pool;
